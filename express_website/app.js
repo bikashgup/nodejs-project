baa32b27b8c160467ba5d1bdcd7cd8f5e3ac23b5
bikash <bikashgup09@gmail.com>
2019-01-09 10:41:22 +0545

    learning views and routing in express

--- express_website/app.js
+++ express_website/app.js
@@ -17,5 +17,13 @@ app.get('/', function(req, res){
     res.render("index");
 });
 
+app.get('/about', function(req, res){
+    res.render("about");
+});
+
+app.get('/contact', function(req, res){
+    res.render("contact");
+});
+
 app.listen(5000);
 console.log("Server is running in port 3000")
